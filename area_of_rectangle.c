6d6b14e3cbd33df00d86887621fa0cff532f8108
mr-pramod-98 <43513968+mr-pramod-98@users.noreply.github.com>
2019-05-10 09:52:02 +0530

    changed from arrays to structures

--- area_of_rectangle.c
+++ area_of_rectangle.c
@@ -1,8 +1,26 @@
 #include<stdio.h>
+
+struct points
+{
+	int x, y;
+};
+
+struct rectangle
+{
+	struct points poi[3];
+	float area;
+};
+
+struct rectangle Area(struct rectangle rect)
+{
+	int distence[3];
+	
+}
+
 void main()
 {
-	int i, n, x[10], y[10], dest[2];
-	float area;
+	int i, n;
+	struct rectangle rect[10];
 	printf("enter the value of n = ");
 	scanf("%d",&n);
 	printf("enter the co-ordinates\n");
@@ -10,10 +28,10 @@ void main()
 	{
 		for (i = 0; i < 3; i++ )
 		{
-			scanf("%d%d",&x[i],&y[i]);
+			scanf("%d%d",&rect[j]->x[i],&rect->y[i]);
 		}
-		dest = find_distence(x, y);
-		area = Area(dest);
+		rect[j] = Area(rect[j]);
 	}
+
 	printf("area = ", area);
 }
